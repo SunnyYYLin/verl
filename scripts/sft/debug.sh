21c66980daac318b3be2b3d17b339e1d896ba854
SunnyYYLin <sunnylinyy@outlook.com>
2025-12-04 02:42:57 +0800

    feat: 更新配置文件和安装脚本以支持新的环境变量和路径

--- scripts/sft/debug.sh
+++ scripts/sft/debug.sh
@@ -3,12 +3,14 @@
 set -x
 
 nproc_per_node=1
-save_path="$HOME/verl/ckpts/HybriDNA-300M-base"
+DATASET_DIR=$DATASETS/verl/gsm8k
+MODEL_DIR=$MODELS/HybriDNA-300M-base
+SAVE_DIR=$MODELS/verl/debug
 
-torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
+HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
      -m verl.trainer.fsdp_sft_trainer \
-    data.train_files=$HOME/data/gsm8k/train.parquet \
-    data.val_files=$HOME/data/gsm8k/test.parquet \
+    data.train_files=$DATASET_DIR/train.parquet \
+    data.val_files=$DATASET_DIR/test.parquet \
     data.prompt_key=extra_info \
     data.response_key=extra_info \
     data.prompt_dict_keys=['question'] \
@@ -16,9 +18,9 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     data.micro_batch_size_per_gpu=1 \
     data.max_length=1024 \
     data.truncation=left \
-    model.partial_pretrain="SunnyLin/HybriDNA-300M-base" \
+    model.partial_pretrain=$MODEL_DIR \
     model.trust_remote_code=true \
-    trainer.default_local_dir=$save_path \
+    trainer.default_local_dir=$SAVE_DIR \
     trainer.project_name=gsm8k-sft \
     trainer.experiment_name=gsm8k-sft-gemma-2b-it \
     trainer.total_epochs=2 \
