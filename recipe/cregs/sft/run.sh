d7c7aecb510fe761e9d386e2dc3a89539f76787d
SunnyYYLin <sunnylinyy@outlook.com>
2025-12-25 21:55:03 +0800

    feat: 更新run.sh中的训练参数设置以支持更高的模型性能和灵活性

--- recipe/cregs/sft/run.sh
+++ recipe/cregs/sft/run.sh
@@ -2,25 +2,26 @@
 
 # export target="train"
 # export nproc_per_node=1
-# export max_prompt_length_by_k=4
-# export learning_rate=5e-5
+# export max_prompt_length_by_k=6
+# export learning_rate=1e-4
 # export weight_decay=1e-1
-# export batch_size=128
-# export batch_size_per_gpu=8
+# export batch_size=512
+# export batch_size_per_gpu=16
 # export dataset_dir=$DATASETS/verl/Gene-CRE
 # export model_dir=$MODELS/GENERator-v2-eukaryote-1.2b-instruct
 # export save_freq=50
 # export test_freq=50
-# export epochs=2
+# export epochs=4
+# export dtype=bf16
 
 set -x
 
-EXPERIMENT_NAME="${target}-32k-bs${batch_size}_p${batch_size_per_gpu}-lr${learning_rate}-wd${weight_decay}-gpu${nproc_per_node}"
+EXPERIMENT_NAME="${target}-${max_prompt_length_by_k}k-bs${batch_size}_p${batch_size_per_gpu}-lr${learning_rate}-wd${weight_decay}-gpu${nproc_per_node}"
 MODEL_BASE=$(basename "$model_dir")
 SAVE_DIR=$MODELS/verl/$MODEL_BASE-$EXPERIMENT_NAME
 MAX_PROMPT_LENGTH=$(( max_prompt_length_by_k * 1024 ))
 
-HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
+HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 \
      -m verl.trainer.fsdp_sft_trainer \
      data.train_files=$dataset_dir/train.parquet \
      data.val_files=$dataset_dir/val.parquet \
@@ -31,21 +32,22 @@ HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_
      data.train_batch_size=$batch_size \
      data.micro_batch_size_per_gpu=$batch_size_per_gpu \
      data.max_length=$MAX_PROMPT_LENGTH \
-     data.truncation=right \
+     data.truncation=error \
      model.partial_pretrain=$model_dir \
      model.trust_remote_code=true \
+     model.fsdp_config.model_dtype=$dtype \
      model.fsdp_config.cpu_offload=false \
      model.fsdp_config.offload_params=false \
      model.enable_gradient_checkpointing=true \
      optim.lr=$learning_rate \
      optim.weight_decay=$weight_decay \
      optim.lr_scheduler=cosine \
-     trainer.save_freq=50 \
-     trainer.test_freq=50 \
+     trainer.save_freq=$save_freq \
+     trainer.test_freq=$test_freq \
      trainer.default_local_dir=$SAVE_DIR \
      trainer.project_name=$MODEL_BASE \
      trainer.experiment_name=$EXPERIMENT_NAME \
-     trainer.total_epochs=2 \
+     trainer.total_epochs=$epochs \
      trainer.resume_mode=auto \
      trainer.nnodes=1 \
      trainer.n_gpus_per_node=$nproc_per_node \
